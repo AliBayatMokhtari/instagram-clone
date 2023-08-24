1e158225b5825d13c2c9cff12cb2972c33bf1677
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2023-08-24 23:24:10 +0430

    [fix][core] remove panda files

--- src/lib/withServices.js
+++ src/lib/withServices.js
@@ -6,6 +6,7 @@ const httpErrorHandlerService = getService(serviceNames.httpErrorHandler)
 const storageService = getService(serviceNames.storage)
 
 // IMPORTANT: this is our Dependency Injection (DI) approach
+// TODO: It's better to get the service names which developer wants to inject
 function withServices(fn) {
 	return fn({
 		httpService,
